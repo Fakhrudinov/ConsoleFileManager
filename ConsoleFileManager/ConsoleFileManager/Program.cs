817289cf693c77b8582c8f195d338e7a9d72b24c
Alexander Fakhrudinov <asbuka@gmail.com>
2021-03-19 06:00:42 +0300

    Add empty  visual studio project

---
+++ ConsoleFileManager/ConsoleFileManager/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ConsoleFileManager
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             /*
+             * Alexander Fakhrudinov = Александр Фахрудинов
+             * asbuka@gmail.com
+             */
+        }
+    }
+}
